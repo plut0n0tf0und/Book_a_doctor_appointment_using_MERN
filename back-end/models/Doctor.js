71cc6fc3d8250df5253691d4125d156e12804c0e
VIGNESH B <100135610+plut0n0tf0und@users.noreply.github.com>
2024-11-18 10:31:55 +0530

    Delete back-end/models directory

--- back-end/models/Doctor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const doctorSchema = new mongoose.Schema({
-  name: String,
-  specialty: String,
-  availableSlots: [String], // or however you defined it
-});
-
-module.exports = mongoose.model('Doctor', doctorSchema);
