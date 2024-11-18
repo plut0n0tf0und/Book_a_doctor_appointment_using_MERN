7511ef0966ac5464051598c8f4b3eaaadeab7be8
VIGNESH B <100135610+plut0n0tf0und@users.noreply.github.com>
2024-11-18 10:31:13 +0530

    Add files via upload

---
+++ back-end/middlewares/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+
+const verifyToken = async (req, res, next) => {
+  try {
+    const authorizationHeader = req.headers["authorization"];
+    if (!authorizationHeader) {
+      return res
+        .status(401)
+        .send({ message: "Authorization header missing", success: false });
+    }
+
+    const token = authorizationHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWT_KEY, (err, decode) => {
+      if (err) {
+        return res
+          .status(200)
+          .send({ message: "Token is not valid", success: false });
+      } else {
+        req.body.userId = decode.id;
+        next();
+      }
+    });
+  } catch (error) {
+    console.error(error); // Handle or log the error appropriately
+    res.status(500).send({ message: "Internal server error", success: false });
+  }
+};
+
+export default verifyToken;
